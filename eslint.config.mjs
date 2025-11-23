0bb2972f3d05e67f7ae07979df165e084b52c8ba
shuto-pro <153715837+shuto-bgn@users.noreply.github.com>
2025-11-23 13:22:06 +0900

    Update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,29 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js"
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+  {
+    languageOptions: {
+      globals: globals.node
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  // rules ここから -----------------------
+  {
+    rules: {
+      "react/react-in-jsx-scope": 0,
+      "semi": [2, "never"],
+      "comma-dangle": [2, "never"]
+    }
+  }
+  // rules ここまで -----------------------
+]
