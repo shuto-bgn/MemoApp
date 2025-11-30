05b5c77e92796f08bf461f270c1c8e9cb5dd4a04
shuto-pro <153715837+shuto-bgn@users.noreply.github.com>
2025-11-30 18:30:31 +0900

    Create memo create

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { Redirect } from 'expo-router'
 import { JSX } from 'react'
 
 const Index = (): JSX.Element => {
-    return <Redirect href='memo/edit'/>
+    return <Redirect href='memo/create'/>
 }
 
 export default Index
